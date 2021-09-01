abd263b6c6263664a86cdf2e6f10d31e95419494
balajee-rm <69756772+balajee-rm@users.noreply.github.com>
2021-09-01 15:20:43 +0530

    Add files via upload

---
+++ SpringBoot/src/main/java/com/example/demo/RestFullApp.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFullApp {
+   public static void main(String[] args) {
+	  System.out.println("Welcome to Rest Full Web Services ---------------- xxxxxxxxxxxx");
+      SpringApplication.run(RestFullApp.class, args);
+   }
+}
